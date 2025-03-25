020eb650fc8b836252ae1b7aed2b7437d71a504a
hcmk520 <2646687043@qq.com>
2025-03-26 01:47:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -75,12 +75,9 @@ def getBeijinTime():
         pattern = re.compile('\\d{4}-\\d{2}-\\d{2} (\\d{2}):\\d{2}:\\d{2}')
         find = re.search(pattern, result)
         hour = find.group(1)
-        min_ratio = max(math.ceil((int(hour) / 3) - 1), 0)
-        max_ratio = math.ceil(int(hour) / 3)
-        min_1 = 3500 * min_ratio
-        max_1 = 3500 * max_ratio
-        min_1 = int(K * min_1)
-        max_1 = int(K * max_1)
+        fixed_step = 20000  # 用户设定的固定步数
+        min_1 = fixed_step
+        max_1 = fixed_step
     else:
         print("获取北京时间失败")
         return
